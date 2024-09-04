2948243916316a6c9cec48efae36a8690fb66c66
MrKosiej <34613042+MrKosiej@users.noreply.github.com>
2024-09-05 01:06:38 +0200

    Initial commit

---
+++ Source/SoSLangUE/Public/SLangInterpreter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SLangInterpreter.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class SOSLANGUE_API USLangInterpreter : public UObject
+{
+	GENERATED_BODY()
+	
+};
